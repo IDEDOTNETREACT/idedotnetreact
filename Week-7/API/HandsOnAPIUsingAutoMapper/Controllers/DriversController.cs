8b25749dda674ca69b9c3c0d25e11dd3eebd7358
santuparsi <santuparsi@gmail.com>
2025-07-23 12:30:23 +0530

    commit7_23

---
+++ Week-7/API/HandsOnAPIUsingAutoMapper/Controllers/DriversController.cs
@@ -0,0 +1,130 @@
+﻿using AutoMapper;
+using HandsOnAPIUsingAutoMapper.DTOs;
+using HandsOnAPIUsingAutoMapper.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnAPIUsingAutoMapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private static List<Driver> drivers = new List<Driver>();
+        private readonly IMapper _mapper;
+
+        public DriversController(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetDrivers()
+        //{
+        //    var items = drivers.Where(x => x.Status == 1).ToList();
+        //    return Ok(items);
+        //}
+        [HttpGet]
+        public IActionResult GetDrivers()
+        {
+            var items = drivers.Where(x => x.Status == 1).ToList();
+            var driverList = _mapper.Map<List<DriverDto>>(items);
+            return Ok(driverList);
+        }
+        //[HttpPost]
+        //public IActionResult CreateDriver(Driver data)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        drivers.Add(data);
+
+        //        return CreatedAtAction("GetDriver", new { data.Id }, data);
+        //    }
+
+        //    return new JsonResult("Something went wrong") { StatusCode = 500 };
+        //}
+        //[HttpPost]
+        //public IActionResult CreateDriver(DriverDto data)
+        //{
+        //    var _driver = new Driver()
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        Status = 1,
+        //        DateAdded = DateTime.Now,
+        //        DateUpdated = DateTime.Now,
+        //        DriverNumber = data.DriverNumber,
+        //        FirstName = data.FirstName,
+        //        LastName = data.LastName,
+        //        WorldChampionships = data.WorldChampionships
+        //    };
+        //    if (ModelState.IsValid)
+        //    {
+        //        drivers.Add(_driver);
+
+        //        return CreatedAtAction("GetDriver", new { _driver.Id }, data);
+        //    }
+
+        //    return new JsonResult("Something went wrong") { StatusCode = 500 };
+        //}
+        [HttpPost]
+        public IActionResult CreateDriver(DriverDto data)
+        {
+            var _driver = _mapper.Map<Driver>(data);
+            _driver.Status = 1;
+            _driver.DateUpdated= DateTime.Now;
+            _driver.DateAdded= DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                drivers.Add(_driver);
+
+                return CreatedAtAction("GetDriver", new { _driver.Id }, data);
+            }
+
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetDriver(Guid id)
+        {
+            var item = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateDriver(Guid id, Driver item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+
+            var existItem = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            existItem.FirstName = item.FirstName;
+            existItem.LastName = item.LastName;
+            existItem.DriverNumber = item.DriverNumber;
+            existItem.WorldChampionships = item.WorldChampionships;
+
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDriver(Guid id)
+        {
+            var existItem = drivers.FirstOrDefault(x => x.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            existItem.Status = 0;
+
+            return Ok(existItem);
+        }
+    }
+}
---
+++ Week-7/API/HandsOnAPIUsingAutoMapper/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using HandsOnAPIUsingAutoMapper.Models;
+using HandsOnAPIUsingAutoMapper.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnAPIUsingAutoMapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            // Populate the user details from DB
+            //var user = GetUserDetails();
+
+            //UserDto userDto = _mapper.Map<UserDto>(user);
+
+            //return Ok(userDto);
+            var user= new User()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.Smith@gmail.com",
+                Address = new Address()
+                {
+                    Country = "US"
+                }
+            };
+            var userDTO=_mapper.Map<UserDto>(user); //convert user data to userDto
+            //var userDTO = new UserDto()
+            //{
+            //    FullName = user.FirstName + " " + user.LastName,
+            //    Email = user.Email,
+            //    AddressCountry = user.Address.Country
+            //};
+            return Ok(userDTO);
+        }
+
+        private static User GetUserDetails()
+        {
+            return new User()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.Smith@gmail.com",
+                Address = new Address()
+                {
+                    Country = "US"
+                }
+            };
+        }
+    }
+}
